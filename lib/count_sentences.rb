a52e106473dc3cd24800a548c4a0bcca43987012
Brendan Baird <brendan.b517@gmail.com>
2023-02-08 20:50:07 -0500

    commit tree b6e377d743c7aa7b89fd0aa55a993b7a4c8ddff1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? "."
   end
 
   def question?
-
+    self.end_with? "?"
   end
 
   def exclamation?
-
+    self.end_with? "!"
   end
 
   def count_sentences
-
+    sentences = self.split ".", "?", "!"
+    sentences.select{|sentence| sentence != ""}.size
   end
 end
+
+p "The weather outside is frightful.".sentence?
+p "And the fire is so delightful!".exclamation?
+p "But since we've no place to go?".question?
